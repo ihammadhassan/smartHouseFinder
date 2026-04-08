c79c762bc0f2df665f5deb8e1b2df7622af94fd4
Hammad Hassan <hammad.haassn@gmail.com>
2026-04-08 10:21:32 +0500

    Added Register API

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,10 @@ const jwt = require ("jsonwebtoken");
 router.post("/register", async (req, res) => {
     try {
         console.log(req.body);
+        res.status(200).json(req.body);
     } catch(err) {
         res.status(500).json({error: err.message});
     }
 });
+
+module.exports = router;
