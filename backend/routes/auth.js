4fe7e7d2deda2fc9393927c78ad298eadb5299fb
Hammad Hassan <hammad.haassn@gmail.com>
2026-04-08 11:06:57 +0500

    Added Login Routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,13 +6,12 @@ const jwt = require ("jsonwebtoken");
 router.post("/register", async (req, res) => {
     try {
         console.log(req.body);
-        res.status(200).json(req.body);
         const {name, email, password} = req.body;
         const existing = await User.findOne({email});
 
         if (existing) {
             console.log("User already exists");
-            return res.status(400).json({error: "Email already exists"});
+            return res.status(400).json({status: 400, error: "Email already exists"});
         }
 
         const hashed = await bcrypt.hash(password, 10);
@@ -28,11 +27,29 @@ router.post("/register", async (req, res) => {
             {expiresIn: "1d"}
         );
 
-        res.json({token});
+        res.status(200).json({status: 200, token});
 
     } catch(err) {
         console.error("ERROR:", err);
-        res.status(500).json({error: err.message});
+        res.status(500).json({status: 500, error: err.message});
+    }
+});
+
+router.post("/login", async(req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({error: "User not found"});
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) return res.status(400).json({status: 400, error: "Invalid Password"});
+        const token = jwt.sign(
+            {userId: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: "1h"}
+        );
+        res.json({status: 200, userId: user._id, token});
+    } catch (err) {
+        res.status(500).json({status: 500, error: err.message})
     }
 });
 
